e036d7dfb3f64fc956cc5b052338189f03e030ab
debarchan.dash@nettantra.net <debarchan.dash@nettantra.net>
2023-05-04 15:59:07 +0530

    readme file

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
 
 
 export default function App() {
 
-  const buttonClickFunction=()=>{
+  const buttonClickFunction = () => {
+    console.log('first');
     Alert.alert('Alert Title', 'Text added successfully', [
       {
         text: 'Cancel',
         onPress: () => console.log('Cancel Pressed'),
         style: 'cancel',
       },
-      {text: 'OK', onPress: () => console.log('OK Pressed')},
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
     ]);
   }
   return (
@@ -20,18 +21,18 @@ export default function App() {
       <View style={styles.taskWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
-          <Task style={styles.singleitem} text={'This is task1'}/>
-          <Task  text={'This is task2'}/>
-          <Task  text={'This is task4'}/>
-          <Task  text={'This is task3'}/>
+          <Task style={styles.singleitem} text={'This is task1'} />
+          <Task text={'This is task2'} />
+          <Task text={'This is task4'} />
+          <Task text={'This is task3'} />
         </View>
       </View>
       <View style={styles.inputcontainer}>
-        <View style={{width:'70%'}}>
-        <TextInput style={styles.inputfield} editable placeholder='Type your text here...' placeholderTextColor={'gray'}/>
+        <View style={{ width: '70%' }}>
+          <TextInput style={styles.inputfield} editable placeholder='Type your text here...' placeholderTextColor={'gray'} />
         </View>
         <View>
-        <Button color="red" onPress={buttonClickFunction}  title='Add'/>
+          <TouchableOpacity style={styles.submitbutton} onPress={buttonClickFunction}><Text>Add</Text></TouchableOpacity>
         </View>
       </View>
     </View>
@@ -39,40 +40,45 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  inputcontainer:{
-    marginBottom:10,
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'space-around',
-    width:"100%",
-    height:50,
+  inputcontainer: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: "100%",
+    height: 50,
   },
-  inputfield:{
-    borderRadius:10,
-    backgroundColor:"#FFF",
-    color:'black',
-    paddingHorizontal:10,
-    height:50,
+  inputfield: {
+    borderRadius: 10,
+    backgroundColor: "#FFF",
+    color: 'black',
+    paddingHorizontal: 10,
+    height: 50,
   },
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: '#E8EAED',
-    justifyContent:'space-between'
+    justifyContent: 'space-between'
   },
-  taskWrapper:{
-    paddingTop:80,
-    paddingHorizontal:20
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20
   },
-  sectionTitle:{
-    fontSize:24,
-    fontWeight:'bold'
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
   },
-  items:{
-    marginTop:10,
+  items: {
+    marginTop: 10,
   },
-  singleitem:{
-    marginVertical:10,
-    backgroundColor:'blue',
+  singleitem: {
+    marginVertical: 10,
+    backgroundColor: 'blue',
+  },
+  submitbutton: {
+    height:50,
+    width:60,
+    backgroundColor: 'yellow',
   }
 });
 
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react_native_todo_app
 This is my first react native app which i am doing by myself for self devlopment purpose it will help me with basic native app components and basic app ui design 
 
+## Install all the dependencies
+***npm install**
